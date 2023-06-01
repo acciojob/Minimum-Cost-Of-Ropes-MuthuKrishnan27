d40de069e4446c2ffab50cfc9fa4b1bc3e13f7d8
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-06-01 16:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+  var input = document.getElementById("rope-lengths").value;
+  var ropes = input.split(",").map(Number);
+
+  // Function to find the minimum cost of connecting ropes
+  function minimumCostOfRopes(ropes) {
+    var totalCost = 0;
+
+    // Create a priority queue to store the ropes' lengths
+    var PriorityQueue = require('js-priority-queue');
+    var queue = new PriorityQueue({ comparator: function(a, b) { return a - b; } });
+
+    // Add ropes to the priority queue
+    for (var i = 0; i < ropes.length; i++) {
+      queue.enqueue(ropes[i]);
+    }
+
+    // Connect the ropes until only one rope is left
+    while (queue.length > 1) {
+      // Extract the two smallest ropes from the queue
+      var rope1 = queue.dequeue();
+      var rope2 = queue.dequeue();
+
+      // Connect the two ropes
+      var connectedRope = rope1 + rope2;
+
+      // Add the cost of connecting to the total cost
+      totalCost += connectedRope;
+
+      // Add the connected rope back to the queue
+      queue.enqueue(connectedRope);
+    }
+
+    return totalCost;
+  }
+
+  // Find the minimum cost of connecting the ropes
+  var minCost = minimumCostOfRopes(ropes);
+
+  // Update the HTML with the minimum cost
+  document.getElementById("result").innerHTML = minCost;
+}
+
